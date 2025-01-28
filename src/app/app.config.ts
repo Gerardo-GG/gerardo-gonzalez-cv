8b008d50dedb248a6ee57c53e47e383b19ba4bff
Gerardo-GG <gerardogonzalezdocs@gmail.com>
2025-01-28 00:32:34 -0600

    Firebase config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: "gerardo-gonzalez-cv", 
+      appId: "1:487614321162:web:476ca5c870a6c5ee1dc5f3", 
+      storageBucket: "gerardo-gonzalez-cv.firebasestorage.app", 
+      apiKey: "AIzaSyC_SCBmXMUgurN3Ar718o3mkzahj4iTAYw", 
+      authDomain: "gerardo-gonzalez-cv.firebaseapp.com", 
+      messagingSenderId: "487614321162", 
+      measurementId: "G-5BYC5DX0Y9" 
+    })), 
+    provideAnalytics(() => getAnalytics()), 
+    ScreenTrackingService
+  ]
 };
