4a4df2ba65f81691c18b9ca17325c49035729c9a
Gerardo-GG <gerardogonzalezdocs@gmail.com>
2025-01-29 00:58:44 -0600

    About section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
+@Component( {
+    selector: 'app-root',
+    imports: [ RouterOutlet, AboutComponent ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+} )
 export class AppComponent {
-  title = 'gerardo-gonzalez-cv';
+    title = 'gerardo-gonzalez-cv';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,19 +6,19 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp({ 
-      projectId: "gerardo-gonzalez-cv", 
-      appId: "1:487614321162:web:476ca5c870a6c5ee1dc5f3", 
-      storageBucket: "gerardo-gonzalez-cv.firebasestorage.app", 
-      apiKey: "AIzaSyC_SCBmXMUgurN3Ar718o3mkzahj4iTAYw", 
-      authDomain: "gerardo-gonzalez-cv.firebaseapp.com", 
-      messagingSenderId: "487614321162", 
-      measurementId: "G-5BYC5DX0Y9" 
-    })), 
-    provideAnalytics(() => getAnalytics()), 
-    ScreenTrackingService
-  ]
+    providers: [
+        provideZoneChangeDetection( { eventCoalescing: true } ),
+        provideRouter( routes ),
+        provideFirebaseApp( () => initializeApp( {
+            apiKey: "AIzaSyC_SCBmXMUgurN3Ar718o3mkzahj4iTAYw",
+            authDomain: "gerardo-gonzalez-cv.firebaseapp.com",
+            projectId: "gerardo-gonzalez-cv",
+            storageBucket: "gerardo-gonzalez-cv.firebasestorage.app",
+            messagingSenderId: "487614321162",
+            appId: "1:487614321162:web:476ca5c870a6c5ee1dc5f3",
+            measurementId: "G-5BYC5DX0Y9"
+        } ) ),
+        provideAnalytics( () => getAnalytics() ),
+        ScreenTrackingService
+    ]
 };
